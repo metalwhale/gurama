4bbdead284c228410f643307698bea77df1dffe1
Metal Whale <metalwhale.dev@gmail.com>
2023-06-19 12:55:27 +0000

    Read model and run inference.

--- README.md
+++ README.md
@@ -4,5 +4,17 @@ A cute llama that helps you correct English grammar.
   <img src="https://github.com/metalwhale/gurama/blob/main/icon.jpg" width="128" height="128" />
 </p>
 
+## Development
+1. Download [`openbuddy-openllama-7b-v5-q4_0.bin`](https://huggingface.co/metalwhale/openbuddy-openllama-7b-v5-q4_0/blob/main/openbuddy-openllama-7b-v5-q4_0.bin) file and put it into [`./model`](./model/) directory
+2. Start and get inside the container:
+    ```bash
+    cd ./infra-dev
+    docker-compose up -d
+    docker-compose exec -it app bash
+    ```
+
 ## Kudos
+- [OpenBuddy - Open Multilingual Chatbot](https://huggingface.co/OpenBuddy/openbuddy-openllama-7b-v5-fp16)
+- [llama.cpp](https://github.com/ggerganov/llama.cpp)
+- [Large Language Models for Everyone, in Rust](https://github.com/rustformers/llm)
 - The icon was generated on https://dreamlike.art using `Dreamlike Anime 1.0` model
--- gurama/src/main.rs
+++ gurama/src/main.rs
@@ -1,3 +1,50 @@
+use llm::Model;
+use std::env;
+use std::io::Write;
+use std::sync::Arc;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        return;
+    }
+    let prompt = &args[1];
+    let llama = llm::load::<llm::models::Llama>(
+        std::path::Path::new("/usr/src/app/model/openbuddy-openllama-7b-v5-q4_0.bin"),
+        llm::VocabularySource::Model,
+        Default::default(),
+        llm::load_progress_callback_stdout,
+    )
+    .unwrap_or_else(|err| panic!("Failed to load model: {err}"));
+    let mut session = llama.start_session(Default::default());
+    let res = session.infer::<std::convert::Infallible>(
+        &llama,
+        &mut rand::thread_rng(),
+        &llm::InferenceRequest {
+            prompt: prompt.into(),
+            parameters: &llm::InferenceParameters {
+                n_threads: num_cpus::get(),
+                sampler: Arc::new(llm::samplers::TopPTopK {
+                    temperature: 0.01,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            play_back_previous_tokens: false,
+            maximum_token_count: None,
+        },
+        &mut Default::default(),
+        |r| match r {
+            llm::InferenceResponse::PromptToken(t) | llm::InferenceResponse::InferredToken(t) => {
+                print!("{t}");
+                std::io::stdout().flush().unwrap();
+                Ok(llm::InferenceFeedback::Continue)
+            }
+            _ => Ok(llm::InferenceFeedback::Continue),
+        },
+    );
+    match res {
+        Ok(result) => println!("\n\nInference stats:\n{result}"),
+        Err(err) => println!("\n{err}"),
+    }
 }
